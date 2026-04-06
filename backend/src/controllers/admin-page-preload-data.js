0c98c6aa8a592d59588e8501450257ec08386351
Sanika <sanikaglsincha@gmail.com>
2026-04-07 00:01:40 +0530

    updated admin-page-preload-data controller

---
+++ backend/src/controllers/admin-page-preload-data.js
@@ -0,0 +1,16 @@
+const candidate = require("../models/candidate");
+const preload_data =async (req,res)=>{
+  try{ 
+       const candidate_data = await candidate.find({status:"pending"});
+       res.status(200).json(candidate_data);
+       console.log(candidate_data);
+    }catch(error){
+        res.status(500).json({
+            message:"error fetchting the candidate-list"
+        })
+    }
+}
+
+module.exports={
+    preload_data
+}  
