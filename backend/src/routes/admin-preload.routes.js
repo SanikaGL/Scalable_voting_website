9822ea9487390578871355ccfa914934c232d290
Sanika <sanikaglsincha@gmail.com>
2026-04-06 23:59:48 +0530

    updated admin-preload route with middlewear to verify admin

--- backend/src/routes/admin-preload.routes.js
+++ backend/src/routes/admin-preload.routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
-// const admin_verify = require("../middleware/admin.token.verify");
-const {preload_data} = require("../controllers/preload-data")
+const { admin_token_verify, admin_Only} = require("./middleware/admin.token.verify.js");
+const {preload_data} = require("../controllers/admin-page-preload-data")
 const router = express.Router();
-router.get("/preload-candidate",preload_data);
+router.get("/preload-candidate",admin_token_verify,admin_Only,preload_data);
 module.exports=router;
  //  ,admin_verify.admin_token_verify,admin_verify.admin_Onl
- //add the above after testing 
+ //  add the above after testing 
