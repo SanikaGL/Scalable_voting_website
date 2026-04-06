7dd6e49f789c21e37d617a2f3bb6b461c885cbed
Sanika <sanikaglsincha@gmail.com>
2026-04-06 23:36:15 +0530

    added file to handle  preloading voter page data

---
+++ backend/src/routes/voter-page-load.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const {voterPage_preload_data} = require("../controllers/voting-page-peload-data")
+const { voter_candidate_token_verify, voter_and_candidate_Only} = require("./middleware/voter-candidate.token.verify.js");
+router.get("/voterPage-preload-candidate",voter_candidate_token_verify, voter_and_candidate_Only,voterPage_preload_data);
+module.exports=router;
